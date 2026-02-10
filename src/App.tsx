adcbc511ba569f178cbf5bf8ecc1ff1046b8d553
Hrushi Kuchekar <hrushikeshkuchekar092@gmail.com>
2026-02-10 23:32:14 +0530

    push other dev code

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ function App() {
    <h1>Hello Hrushikesh</h1>
    <h3>login Branch update stash cahnegs   </h3>
    <h2>After the 1st stash</h2>
+
+   <h3>Atharva is comming !!</h3>
+   <h5>Hrushi is sleeping !!</h5>
    </>
   )
 }
