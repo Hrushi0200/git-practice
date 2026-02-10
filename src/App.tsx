c15a916309573aa9f8c3acc5e0b768732fe0cc37
Hrushi Kuchekar <hrushikeshkuchekar092@gmail.com>
2026-02-10 23:23:30 +0530

    revert-changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
   return (
    <>
    <h1>Hello Hrushikesh</h1>
-   <h3>login Branch update stash cahnegs  </h3>
+   <h3>login Branch update stash cahnegs   </h3>
+   <h2>After the 1st stash</h2>
    </>
   )
 }
